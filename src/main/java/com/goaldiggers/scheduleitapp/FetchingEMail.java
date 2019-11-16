bab3f7310afe603eea336b2639f7f2da12f8d3b9
shruthireddyp8 <46663563+shruthireddyp8@users.noreply.github.com>
2019-11-16 13:05:52 -0500

    cleaned data while fetching the messages

--- src/main/java/com/goaldiggers/scheduleitapp/FetchingEMail.java
+++ src/main/java/com/goaldiggers/scheduleitapp/FetchingEMail.java
@@ -23,12 +23,13 @@ import javax.mail.Session;
 import javax.mail.Store;
 
 public class FetchingEMail {
+	static int counter = 0;
     public static void fetch(String pop3Host, String storeType, String user,
             String password) {
         try {
 
             Properties properties = new Properties();
-            PrintWriter writer = new PrintWriter("the-file-name.txt", "UTF-8");
+            
 
             String host = "pop.outlook.com";
             Session session = Session.getDefaultInstance(properties);
@@ -48,22 +49,17 @@ public class FetchingEMail {
             System.out.println("messages.length---" + messages.length);
 
             for (int i = 0; i < messages.length; i++) {
+            	PrintWriter writer = new PrintWriter("the-file-name-"+i+".txt", "UTF-8");
                 Message message = messages[i];
                 System.out.println("---------------------------------");
                 writePart(message,writer);
-                String line = reader.readLine();
-                if ("YES".equals(line)) {
-                    message.writeTo(System.out);
-                }
-                else if ("QUIT".equals(line)) {
-                    break;
-                }
+                writer.close();
             }
 
             // close the store and folder objects
             emailFolder.close(false);
             store.close();
-            writer.close();
+            
 
         }
         catch (NoSuchProviderException e) {
@@ -109,8 +105,17 @@ public class FetchingEMail {
             System.out.println("This is plain text");
             System.out.println("---------------------------");
             String plainText = (String) p.getContent();
-            System.out.println(plainText);
-            writer.println(plainText);
+            String s[] = plainText.split("\\n");
+            String tempPlainText = "";
+            for(int i=0;i<=s.length-1;i++) {
+            	s[i] = s[i].replace('*', '\t');
+            	s[i] = s[i].replace(',', '\t');
+            	if(!(s[i].contains("http") || s[i].contains("ttps")) && !(s[i].contains("Image") || s[i].contains("image")) && !(s[i].equals("\r"))) {
+            		tempPlainText = tempPlainText+" "+s[i];
+            	}
+            }
+            writer.println(tempPlainText);
+            counter++;
         }
         // check if the content has attachment
         else if (p.isMimeType("multipart/*")) {
@@ -128,57 +133,7 @@ public class FetchingEMail {
             System.out.println("---------------------------");
             writePart((Part) p.getContent(),writer);
         }
-        // check if the content is an inline image
-        /*
-         * else if (p.isMimeType("image/jpeg")) {
-         * System.out.println("--------> image/jpeg"); Object o =
-         * p.getContent();
-         * 
-         * InputStream x = (InputStream) o; // Construct the required byte array
-         * System.out.println("x.length = " + x.available()); while ((i =
-         * x.available()) > 0) { int result = (int) (x.read(bArray)); if (result
-         * == -1) int i = 0; byte[] bArray = new byte[x.available()];
-         * 
-         * break; } FileOutputStream f2 = new
-         * FileOutputStream("/tmp/image.jpg"); f2.write(bArray); }
-         */
-        /*else if (p.getContentType().contains("image/")) {
-            System.out.println("content type" + p.getContentType());
-            File f = new File("image" + new Date().getTime() + ".jpg");
-            DataOutputStream output = new DataOutputStream(
-                    new BufferedOutputStream(new FileOutputStream(f)));
-            com.sun.mail.util.BASE64DecoderStream test = (com.sun.mail.util.BASE64DecoderStream) p
-                    .getContent();
-            byte[] buffer = new byte[1024];
-            int bytesRead;
-            while ((bytesRead = test.read(buffer)) != -1) {
-                output.write(buffer, 0, bytesRead);
-            }
-        }*/
-        else {
-            Object o = p.getContent();
-            if (o instanceof String) {
-                System.out.println("This is a string");
-                System.out.println("---------------------------");
-                System.out.println((String) o);
-            }
-            else if (o instanceof InputStream) {
-                System.out.println("This is just an input stream");
-                System.out.println("---------------------------");
-                InputStream is = (InputStream) o;
-                is = (InputStream) o;
-                int c;
-                while ((c = is.read()) != -1) {
-                    System.out.write(c);
-                }
-            }
-            else {
-                System.out.println("This is an unknown type");
-                System.out.println("---------------------------");
-                System.out.println(o.toString());
-            }
-        }
-
+        
     }
 
     /*
